79aeb4587c4f2ec33a8e9c08da091647f5159c92
lmjing <lmjing4905@gmail.com>
2021-09-01 23:55:23 +0900

    3.0 Class Components and State

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -1,57 +1,24 @@
 import './App.css';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
+import React from 'react';
 
-const foodIlike = [
-  {
-    id: 1,
-    name: "kimchi",
-    picture: "https://encrypted-tbn0.gstatic.com/pictures?q=tbn:ANd9GcTFyLqn-fju8k3OD_cgyK51g8qRrcWiOFqMzFngX79bKDwbq4dpmKIL55kisWXyWtoOXwjZI3k&usqp=CAc",
-    rating: 4.5
-  },
-  {
-    id: 2,
-    name: "ramen",
-    picture: "https://mblogthumb-phinf.pstatic.net/MjAyMDA1MjZfNDMg/MDAxNTkwNDgxOTc1OTE1.rpezhYUxGHG0X6-dmVwuACnqm7AugH9CUjxEatcVNsAg.C27glH_kXPk5zTORLyjUbU_yjkGDEbmwZXjaq_xGltIg.JPEG.naverschool/%ED%98%B8%EB%A1%9C%EB%A1%9C%EB%A1%9D.jpg?type=w800",
-    rating: 4.7
-  },
-  {
-    id: 3,
-    name: "samgiopsal",
-    picture: "https://pds.joins.com/news/component/htmlphoto_mmdata/201702/27/117f5b49-1d09-4550-8ab7-87c0d82614de.jpg",
-    rating: 4.1
-  },
-  {
-    id: 4,
-    name: "doncasu",
-    picture: "https://www.ypsori.com/news/photo/201811/14515_14466_1033.jpg",
-    rating: 3.2
+class App extends React.Component {
+  state = {
+    count: 0
+  };
+  add = () => {
+    console.log("add");
   }
-]
-
-function Food({ name, picture, rating }) {
-  return <div>
-    <h2>I love {name}</h2>
-    <div> {rating}/5.0</div>
-    <img src={picture} alt={name} />
-  </div>;
-}
-
-Food.propTypes = {
-  name: PropTypes.string.isRequired,
-  picture: PropTypes.string.isRequired,
-  rating: PropTypes.number
-}
-
-function renderFood(dish) {
-  return <Food key={dish.id} name={dish.name} picture={dish.picture} rating={dish.rating}/>
-}
-
-function App() {
-  return (
-    <div className="App">
-      {foodIlike.map(renderFood)}
+  minus = () => {
+    console.log("minus");
+  }
+  render() {
+    return <div>
+      <h1>The number is {this.state.count}</h1>
+      <button onClick={this.add}>add</button>
+      <button onClick={this.minus}>minus</button>
     </div>
-  );
+  }
 }
 
 export default App;
