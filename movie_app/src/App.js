9fe17fbf06237d8f5abe031520a11376597f114f
lmjing <lmjing4905@gmail.com>
2021-09-02 23:54:23 +0900

    3.2 Component Life Cycle

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -3,6 +3,10 @@ import './App.css';
 import React from 'react';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    console.log("hello")
+  }
   state = {
     count: 0
   };
@@ -12,7 +16,17 @@ class App extends React.Component {
   minus = () => {
     this.setState(cur => ({count: cur.count - 1}))
   };
+  componentDidMount() {
+    console.log("Component rendered");
+  }
+  componentDidUpdate() {
+    console.log("I just updated");
+  }
+  componentWillUnmount() {
+    console.log("Goodbye, cruel world");
+  }
   render() {
+    console.log("I`m rendering")
     return <div>
       <h1>The number is {this.state.count}</h1>
       <button onClick={this.add}>add</button>
