8769ae18c18c702c9a37fc11fdb930b514e99fb9
lmjing <lmjing4905@gmail.com>
2021-09-14 16:09:44 +0900

    2.3 usePreventLeave

---
+++ 2_react-hooks-introduction/07. usePreventLeave.js
@@ -0,0 +1,36 @@
+/*
+hook 미사요
+*/
+import { StrictMode } from "react";
+import ReactDOM from "react-dom";
+import "./styles.css";
+
+const usePreventLeave = () => {
+    const listener = (e) => {
+        e.preventDefault();
+        e.returnValue = "";
+    };
+    const enablePrevent = () => window.addEventListener("beforeunload", listener);
+    const diablePrevent = () =>
+        window.removeEventListener("beforeunload", listener);
+    return { enablePrevent, diablePrevent };
+};
+
+const App = () => {
+    const { enablePrevent, diablePrevent } = usePreventLeave();
+    return (
+        <div className="App">
+            <button onClick={enablePrevent}>Protect</button>
+            <button onClick={diablePrevent}>Unprotect</button>
+        </div>
+    );
+}
+
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+    <StrictMode>
+        <App />
+    </StrictMode>,
+    rootElement
+);
