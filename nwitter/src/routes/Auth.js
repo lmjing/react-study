cd9ad15344a8b37e336b3a7628eee7fc2a685d93
lmjing <lmjing4905@gmail.com>
2021-09-18 17:01:20 +0900

    2.1 Login Form part One & 2.2 recap

--- nwitter/src/routes/Auth.js
+++ nwitter/src/routes/Auth.js
@@ -1,5 +1,29 @@
 /* eslint-disable import/no-anonymous-default-export */
-import React from "react";
+import React, { useState } from "react";
 
-const Auth = () => <span>Auth</span>;
+const Auth = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const onChange = (event) => {
+        const { target: { name, value } } = event;
+        if (name === "email") setEmail(value);
+        else setPassword(value);
+    }
+    const onSubmit = (event) => {
+        event.preventDefault();
+    }
+
+    return (
+        <div>
+            <form onSubmit={onSubmit}>
+                <input type="email" name="email" placeholder="email" required value={email} onChange={onChange} />
+                <input type="password" name="password" placeholder="password" required value={password} onChange={onChange} />
+                <input type="submit" value="Log In" />
+            </form>
+            <button>Continue with Google</button>
+            <button>Continue with Github</button>
+        </div>
+    )
+}
 export default Auth;
