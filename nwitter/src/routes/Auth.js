d107c2badc12f86d8b17f735c3b0070ea4f33c09
lmjing <lmjing4905@gmail.com>
2021-09-19 22:49:18 +0900

    2.3 Creating Account

--- nwitter/src/routes/Auth.js
+++ nwitter/src/routes/Auth.js
@@ -1,17 +1,32 @@
 /* eslint-disable import/no-anonymous-default-export */
 import React, { useState } from "react";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import { authService } from "firebase";
 
 const Auth = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [newAccont] = useState(true);
 
     const onChange = (event) => {
         const { target: { name, value } } = event;
         if (name === "email") setEmail(value);
         else setPassword(value);
     }
-    const onSubmit = (event) => {
+    const onSubmit = async (event) => {
         event.preventDefault();
+
+        try {
+            let data;
+            if (newAccont) {
+                data = await createUserWithEmailAndPassword(authService, email, password);
+            } else {
+                data = await signInWithEmailAndPassword(authService, email, password);
+            }
+            console.log(data);
+        } catch (err) {
+            console.log(err);
+        }
     }
 
     return (
@@ -19,7 +34,7 @@ const Auth = () => {
             <form onSubmit={onSubmit}>
                 <input type="email" name="email" placeholder="email" required value={email} onChange={onChange} />
                 <input type="password" name="password" placeholder="password" required value={password} onChange={onChange} />
-                <input type="submit" value="Log In" />
+                <input type="submit" value={newAccont ? 'Create Account' : 'Log In'} />
             </form>
             <button>Continue with Google</button>
             <button>Continue with Github</button>
