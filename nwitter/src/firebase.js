aef2fecb742014b470e05fd14241275b52043f1b
lmjing <lmjing4905@gmail.com>
2021-09-20 17:52:12 +0900

    3.0 Form and Database Setup

--- nwitter/src/firebase.js
+++ nwitter/src/firebase.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged, GoogleAuthProvider, GithubAuthProvider, signInWithPopup, signOut } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
