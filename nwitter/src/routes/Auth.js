f907e3991edab78c6a94775c779962e8831a7033
lmjing <lmjing4905@gmail.com>
2021-10-04 13:24:41 +0900

    6.0 Cleaning JS - AuthForm, NweetFactory 컴포넌트 분리

--- nwitter/src/routes/Auth.js
+++ nwitter/src/routes/Auth.js
@@ -1,34 +1,8 @@
 /* eslint-disable import/no-anonymous-default-export */
-import React, { useState } from "react";
-import { authService, createUserWithEmailAndPassword, signInWithEmailAndPassword, googleProvider, githubProvider, signInWithPopup } from "firebase";
+import AuthForm from "components/AuthForm";
+import { authService, googleProvider, githubProvider, signInWithPopup } from "firebase";
 
 const Auth = () => {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [newAccont, setNewAccount] = useState(true);
-    const [error, setError] = useState("");
-
-    const onChange = (event) => {
-        const { target: { name, value } } = event;
-        if (name === "email") setEmail(value);
-        else setPassword(value);
-    }
-    const onSubmit = async (event) => {
-        event.preventDefault();
-
-        try {
-            let data;
-            if (newAccont) {
-                data = await createUserWithEmailAndPassword(authService, email, password);
-            } else {
-                data = await signInWithEmailAndPassword(authService, email, password);
-            }
-            console.log(data);
-        } catch (error) {
-            setError(error.message);
-        }
-    }
-    const toggleAccount = () => { setNewAccount((prev) => !prev) }
     const onSocialClick = async (event) => {
         const { target: { name } } = event;
 
@@ -38,21 +12,16 @@ const Auth = () => {
         } else if (name === "github") {
             provider = githubProvider;
         }
-        const data = await signInWithPopup(authService, provider);
-        console.log(data);
+        await signInWithPopup(authService, provider);
     }
 
     return (
         <div>
-            <form onSubmit={onSubmit}>
-                <input type="email" name="email" placeholder="email" required value={email} onChange={onChange} />
-                <input type="password" name="password" placeholder="password" required value={password} onChange={onChange} />
-                <input type="submit" value={newAccont ? 'Create Account' : 'Log In'} />
-                {error}
-            </form>
-            <span onClick={toggleAccount}>{newAccont ? 'Log In' : 'Create Account'}</span>
-            <button name="google" onClick={onSocialClick}>Continue with Google</button>
-            <button name="github" onClick={onSocialClick}>Continue with Github</button>
+            <AuthForm />
+            <div>
+                <button name="google" onClick={onSocialClick}>Continue with Google</button>
+                <button name="github" onClick={onSocialClick}>Continue with Github</button>
+            </div>
         </div>
     )
 }
